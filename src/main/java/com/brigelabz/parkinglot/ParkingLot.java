c4feee83a17757f1916c93e03ca30b931549ea5d
PalakPartani <partanipalak16@gmail.com>
2020-03-19 04:38:56 -0700

    ADDED:Testcase to check if driver can unpark the vehicle

--- src/main/java/com/brigelabz/parkinglot/ParkingLot.java
+++ src/main/java/com/brigelabz/parkinglot/ParkingLot.java
@@ -1,7 +1,15 @@
 package com.brigelabz.parkinglot;
 
 public class ParkingLot {
-    public boolean CheckParkVehicle() {
+    private Object vehicle;
+
+    public boolean CheckParkVehicle(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+    public boolean unParkVehicle(Object vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
+        return false;
     }
 }
--- src/test/java/com/brigelabz/parkinglot/ParkingLotTest.java
+++ src/test/java/com/brigelabz/parkinglot/ParkingLotTest.java
@@ -1,15 +1,33 @@
 package com.brigelabz.parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLot = new ParkingLot();
+        vehicle= new Object();
+    }
+
 
     //test for initial status of parking lot
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
         ParkingLot parkingLotSystem = new ParkingLot();
-        boolean isParked = parkingLotSystem.CheckParkVehicle();
+        boolean isParked = parkingLotSystem.CheckParkVehicle(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.CheckParkVehicle(vehicle);
+        boolean isUnParked = parkingLot.unParkVehicle(vehicle);
+        Assert.assertTrue(isUnParked);
     }
 }
